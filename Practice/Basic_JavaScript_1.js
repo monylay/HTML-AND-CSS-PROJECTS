5721835b234bda983db3c98776c9ed58d9dbc1cb
monylay <monylay48@gmail.com>
2026-07-13 17:31:34 -0400

    updated portfolio

--- Practice/Basic_JavaScript_1.js
+++ Practice/Basic_JavaScript_1.js
@@ -3,3 +3,22 @@
 		document.getElementById("Irish").innerHTML =
 		"<span style='color:green'>Kiss me, I'm Irish!</span>";
 	}
+
+
+function validateForm() {
+	let x = document.forms["myForm"]["phone"].value;
+	if (x == "") {
+		alert("Phone number must be filled out");
+		return false;
+	}
+}
+
+function openForm() {
+	document.getElementById("myForm").style.display = "block";
+	document.querySelector(".open-button").style.display = "none";
+}
+
+function closeForm() {
+	document.getElementById("myForm").style.display = "none";
+	document.querySelector(".open-button").style.display = "block";
+}
